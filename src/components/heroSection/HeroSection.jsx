f94dcb9d47047a472d67236c2d8d018d9a44a5d1
MayurPatil369 <epicpanda121@gmail.com>
2023-12-16 11:36:47 +0530

    project

---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div>
+        <img src="https://pngtree.com/freebackground/medical-insurance-flat-geometric-blue-banner_1106244.html" alt="" />
+    </div>
+  )
+}
+
+export default HeroSection
